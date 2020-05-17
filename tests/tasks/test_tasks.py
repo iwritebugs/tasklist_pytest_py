fef72ba0a9ece2986e29b5a3d5bc564b96fdd0f6
Your Name <holmelin@gmail.com>
2020-05-17 10:46:05 +0200

    Up

--- tests/tasks/test_tasks.py
+++ tests/tasks/test_tasks.py
@@ -3,3 +3,14 @@ import tasks
 def test_smoke():
     t1 = tasks.Task()
     assert t1 is not None
+
+def test_defaults():
+    task = tasks.Task()
+    assert task == (None, None, False, None)
+
+def test_member_access():
+    task = tasks.Task('Summary', 'Owner', False, 1)
+    assert task.summary == 'Summary'
+    assert task.owner == 'Owner'
+    assert task.done == False
+    assert task.id == 1
