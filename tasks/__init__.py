b11f96cc8c863cc9e4ac927918747e2d634fc84e
Your Name <holmelin@gmail.com>
2020-05-24 17:50:49 +0000

    version

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,7 @@
 from collections import namedtuple
 
+__version__ = '0.1.0'
+
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
+
--- tests/tasks/test_tasks.py
+++ tests/tasks/test_tasks.py
@@ -1,14 +1,17 @@
 import tasks
 import pytest
 
+
 @pytest.mark.smoke
 def test_smoke():
     t1 = tasks.Task()
     assert t1 is not None
 
+
 def test_defaults():
     task = tasks.Task()
     assert task == (None, None, False, None)
+
 
 def test_member_access():
     task = tasks.Task('Summary', 'Owner', False, 1)
@@ -16,3 +19,9 @@ def test_member_access():
     assert task.owner == 'Owner'
     assert task.done == False
     assert task.id == 1
+
+
+@pytest.mark.skipif(tasks.__version__ >= '0.1.0',
+                    reason='Skipped after version 0.1.0')
+def test_version():
+    assert False
